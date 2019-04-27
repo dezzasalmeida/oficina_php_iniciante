48f9e8ef24367f74937120350d64a6ca9169debf
eilem <eeilemm@gmail.com>
2019-04-27 18:51:59 -0300

    form

---
+++ envio.php
@@ -0,0 +1,20 @@
+<?php
+
+$nome = $_POST['nome'];
+//$nome = $_REQUEST['nome'];
+
+echo ("Nome" . $nome);
+
+$email = $_POST['email'];
+//$email = $_REQUEST['email'];
+// echo = "E-mail" . $email
+
+$telefone = $_POST['telefone'];
+//$telefone = $_REQUEST['telefone'];
+// echo = "Telefone" . $telefone
+
+$mensagem = $_POST['mensagem'];
+$mensagem = $_REQUEST['mensagem'];
+// echo = "Mensagem" . $mensagem
+
+?>
--- index.php
+++ index.php
@@ -10,7 +10,27 @@
         <?php
 
             // Coloque seu código aqui!
+            // PHP tem uma tipagem dinamica (boolean - Integer/Int - float - string com aspas simples(chama texto) ou duplas(chama variavel e texto) - array)
+            // echo "Hello World"; - serve para mostrar o texto na tela
+            // $nome - O $ cria variavel no php
+            // define("MEU_CARRO") - forma de adicionar uma constante
+            // var_dump serve para printar um teste - como o console.log
+            
 
+            $frutas = array( "maça" , "banana" , "laranja");
+
+            var_dump( $frutas);
+
+            echo "<pre>";
+            $frutasMatrix = array( "maça" => array("argentina" , "gala") ,
+                                 "banana" => array("da terra" , "nanica", "maça"),
+                                 "laranja" => array("pera", "lima"));
+
+            var_dump( $frutas);
+            echo("</br>");      
+            echo("</br>");
+            var_dump( $frutasMatrix);
+            
         ?>
 
     </body>
